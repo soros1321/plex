3fb3ef3ddb2249129f5b4fd0190ff3d5895bb9d1
Johan Mulyono <johan@paramation.com>
2018-02-06 15:24:47 +0700

    1. Added share your request URL component

--- dharma/src/modules/RequestLoan/RequestLoanSuccess.tsx
+++ dharma/src/modules/RequestLoan/RequestLoanSuccess.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Header } from '../../components';
 import { GetNotified } from './GetNotified';
+import { ShareRequestURL } from './ShareRequestURL';
 
 interface States {
 	email: string;
@@ -14,6 +15,8 @@ class RequestLoanSuccess extends React.Component<{}, States> {
 		};
 		this.handleEmailChange = this.handleEmailChange.bind(this);
 		this.handleGetNotified = this.handleGetNotified.bind(this);
+		this.handleCopyClipboard = this.handleCopyClipboard.bind(this);
+		this.handleShareSocial = this.handleShareSocial.bind(this);
 	}
 
 	handleEmailChange(email: string) {
@@ -24,11 +27,20 @@ class RequestLoanSuccess extends React.Component<{}, States> {
 		console.log('Get Notified', this.state);
 	}
 
+	handleCopyClipboard() {
+		console.log('Copy to clipboard');
+	}
+
+	handleShareSocial(socialMediaName: string) {
+		console.log('Share social', socialMediaName);
+	}
+
 	render() {
 		return (
 			<div>
 				<Header title={'Next, share your loan request with lenders'} description={'Get lenders to fill your loan request by directing them to your request URL.'} />
 				<GetNotified email={this.state.email} onInputChange={this.handleEmailChange} onFormSubmit={this.handleGetNotified} />
+				<ShareRequestURL requestURL="dharma.io/aUAFSDKjsdf" onCopyClipboard={this.handleCopyClipboard} onShareSocial={this.handleShareSocial} />
 			</div>
 		);
 	}
---
+++ dharma/src/modules/RequestLoan/ShareRequestURL.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import {
+	Row,
+	Col,
+	FormGroup,
+	Label,
+	Input,
+	Button
+} from 'reactstrap';
+import './ShareRequestURL.css';
+
+interface Props {
+	requestURL: string;
+	onCopyClipboard: () => void;
+	onShareSocial: (socialMediaName: string) => void;
+}
+
+class ShareRequestURL extends React.Component<Props, {}> {
+	constructor (props: Props) {
+		super(props);
+		this.handleCopyClipboard = this.handleCopyClipboard.bind(this);
+		this.handleShareSocial = this.handleShareSocial.bind(this);
+	}
+
+	handleCopyClipboard() {
+		this.props.onCopyClipboard();
+	}
+
+	handleShareSocial(socialMediaName: string, e: React.MouseEvent<HTMLDivElement>) {
+		this.props.onShareSocial(socialMediaName);
+	}
+
+	render() {
+		const socialButtons = [
+			{name: 'twitter', imgURL: require('../../assets/img/logo_twitter.png')},
+			{name: 'facebook', imgURL: require('../../assets/img/logo_facebook.png')},
+			{name: 'instagram', imgURL: require('../../assets/img/logo_instagram.png')}
+		];
+		const socialButtonsRow = socialButtons.map((social) => (
+			<div className="share-button" key={social.name} onClick={(e) => this.handleShareSocial(social.name, e)}>
+				<img src={social.imgURL} />
+			</div>
+		));
+		return (
+			<div className="share-request-url-container">
+				<Label for="request-url">Share your request URL</Label>
+				<Row className="gray">
+					<Col xs="12" md="2" className="image-container" />
+					<Col xs="12" md="10">
+						<div className="share-buttons-container">
+							{socialButtonsRow}
+						</div>
+						<FormGroup>
+							<Row>
+								<Col xs="12" md="8">
+									<Input type="text" name="request-url" className="width-95" value={this.props.requestURL} readOnly={true} />
+								</Col>
+								<Col xs="12" md="4">
+									<Button className="button" type="submit" onClick={this.handleCopyClipboard}>Copy</Button>
+								</Col>
+							</Row>
+						</FormGroup>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
+}
+
+export { ShareRequestURL };
