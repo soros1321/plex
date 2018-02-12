cfa40a7ebee2e42bd330a43ae4e3688681d7898a
Christopher Min <chris@medisas.com>
2018-02-11 20:37:08 -0800

    Wrap main application router in Provider

--- dharma/src/index.tsx
+++ dharma/src/index.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { AppRouter } from './router/router';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/index.css';
 
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
 ReactDOM.render(
-  <AppRouter />,
+  <Provider store={store}>
+    <AppRouter />
+   </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
