2591b062291ad0438cdde90635e71aad0bf3288d
Christopher Min <chris@medisas.com>
2018-02-11 22:43:22 -0800

    Connect App to Redux store

--- dharma/src/App.tsx
+++ dharma/src/App.tsx
@@ -1,16 +1,51 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
 import { PageLayout } from './layouts';
+import { Login } from './modules';
+import { logIn } from './actions';
+import { LogInAction } from './constants';
+import { authAPI } from './services';
 
-class App extends React.Component {
+interface AppProps {
+  authenticated: boolean;
+  logIn: () => LogInAction;
+}
+
+export type State = {
+  auth: {
+    authenticated: boolean
+  };
+};
+
+class App extends React.Component<AppProps, {}> {
 	render() {
-		return (
-			<div>
-				<PageLayout>
-					{this.props.children}
-				</PageLayout>
-			</div>
-		);
+    if (!this.props.authenticated) {
+      return (<Login logInAction={this.props.logIn} />);
+    } else {
+      return (
+        <div>
+          <PageLayout>
+            {this.props.children}
+          </PageLayout>
+        </div>
+      );
+    }
 	}
 }
 
-export default App;
+function mapStateToProps(state: State) {
+  const authenticated = state.auth.authenticated || authAPI.loggedIn();
+
+  return {
+    authenticated,
+  };
+}
+
+function mapDispatchToProps(dispatch: Dispatch<State>) {
+  return {
+    logIn: bindActionCreators(logIn, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App) as React.ComponentClass<{}>;
