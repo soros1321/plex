b25c4e1b15a7ad22b11f904a6879216d6b197c73
Johan Mulyono <johan@paramation.com>
2018-02-15 15:40:50 +0700

    If the field is disabled, we want to skip the highlighting

--- src/components/Form/JSONSchemaForm/index.tsx
+++ src/components/Form/JSONSchemaForm/index.tsx
@@ -69,6 +69,9 @@ function highlightNextSibling(el: any, cls: string) {
 					siblingInputField = sibling.querySelector('button');
 				}
 				if (siblingInputField) {
+					if (siblingInputField.disabled) {
+						continue;
+					}
 					potentialSibling = findAncestor(siblingInputField, cls);
 					if (potentialSibling) {
 						siblingInputField.focus();
