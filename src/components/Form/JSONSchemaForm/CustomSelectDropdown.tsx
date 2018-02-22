1e610762a1d788408ae312a62feb889a37dc5963
Johan Mulyono <johan@paramation.com>
2018-02-22 16:42:58 +0700

    Fixed the error where map() is missing key in CustomSelectDropdown component

--- src/components/Form/JSONSchemaForm/CustomSelectDropdown.tsx
+++ src/components/Form/JSONSchemaForm/CustomSelectDropdown.tsx
@@ -13,7 +13,7 @@ export const CustomSelectDropdown = (props: any) => {
 			>
 				{props.placeholder !== '' && (<option>{props.placeholder}</option>)}
 				{props.options.enumOptions.map((opt: any) => (
-					<option value={opt.value}>{opt.label}</option>
+					<option value={opt.value} key={opt.value}>{opt.label}</option>
 				))}
 			</select>
 			<PressEnter className={props.value ? 'active' : ''}>OK, Press ENTER</PressEnter>
