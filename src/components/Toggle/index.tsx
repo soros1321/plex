cd0ff15b11197dfd4c2b5181a89145f7e8ef993e
Christopher Min <chris@medisas.com>
2018-02-23 11:22:18 -0800

    Implement Toggle component

---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import ReactToggle from 'react-toggle';
+import { ToggleLabel, ToggleName } from './styledComponents';
+import './toggle.css';
+
+interface Props {
+  disabled?: boolean;
+  name: string;
+  label: string;
+  prepend?: string;
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+}
+
+interface State {
+  disabled: boolean;
+}
+
+class Toggle extends React.Component<Props, State> {
+  constructor (props: Props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      disabled: props.disabled ? props.disabled : false
+    };
+  }
+
+  handleChange(e: React.FormEvent<HTMLInputElement>) {
+    this.setState({ disabled: true });
+    this.props.onChange(e.currentTarget.checked);
+    this.setState({ disabled: false });
+  }
+
+  render() {
+    return (
+      <div>
+        <ToggleLabel>
+          <ReactToggle
+            checked={this.props.checked}
+            disabled={this.state.disabled}
+            icons={false}
+            id={this.props.prepend ? this.props.prepend + '-' + this.props.name : this.props.name}
+            name={this.props.name}
+            onChange={this.handleChange}
+          />
+          <ToggleName>{this.props.label}</ToggleName>
+        </ToggleLabel>
+      </div>
+    );
+  }
+}
+
+export { Toggle };
---
+++ src/components/Toggle/styledComponents.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ToggleLabel = styled.label`
+  display: flex;
+  align-items: center;
+  padding: 0px 25px;
+  opacity: 1;
+`;
+
+export const ToggleName = styled.div`
+  color: #FFFFFF;
+  display: inline-block;
+  font-family: DIN;
+  font-size: 13px;
+  font-weight: bold;
+  line-height: 25px;
+  margin-left: 10px;
+  opacity: 0.5;
+`;
