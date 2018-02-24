365686fd650baddae0e380e44ffcb7d22fbf6a12
Christopher Min <chris@medisas.com>
2018-02-24 09:04:37 -0800

    Update mockGetProxyAllowanceAsync with hash function

--- __mocks__/dharma.js/token/index.tsx
+++ __mocks__/dharma.js/token/index.tsx
@@ -1,3 +1,16 @@
-export const mockGetProxyAllowanceAsync = jest.fn(async (tokenAddress, ownerAddress) => 11);
+const hashedString = (s: string) => {
+  var hash = 0, i, chr;
+  if (s.length === 0) return hash;
+  for (i = 0; i < s.length; i++) {
+    chr   = s.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
+export const mockGetProxyAllowanceAsync = jest.fn(async (tokenAddress, ownerAddress) => {
+  return new Number(hashedString(`{${tokenAddress}-${ownerAddress}`));
+});
 export const mockSetProxyAllowanceAsync = jest.fn();
 export const mockSetUnlimitedProxyAllowanceAsync = jest.fn();
