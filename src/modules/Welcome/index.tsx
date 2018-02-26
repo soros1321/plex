30b27b8f1b19951491e57da008e205eae27597db
Johan Mulyono <johan@paramation.com>
2018-02-26 15:47:56 +0700

    Fixed welcome page

--- src/modules/Welcome/index.tsx
+++ src/modules/Welcome/index.tsx
@@ -1,16 +1,29 @@
 import * as React from 'react';
 import { PaperLayout } from '../../layouts';
 import { MainWrapper } from '../../components';
+import {
+	BannerContainer,
+	Header,
+	Description,
+	ButtonContainer,
+	SkipButton,
+	NextButton
+} from './styledComponents';
 
 class Welcome extends React.Component<{}, {}> {
 	render() {
 		return (
 			<PaperLayout>
+				<BannerContainer />
 				<MainWrapper>
-					<h1>Meet Dharma</h1>
-					<p>
-						The open protocol for tokenized debt.
-					</p>
+					<Header>Welcome to Dharma Bazaar</Header>
+					<Description>
+						Here's a quick description of what you can do and why it is awesome.
+					</Description>
+					<ButtonContainer>
+						<SkipButton>Skip</SkipButton>
+						<NextButton>Next &rarr;</NextButton>
+					</ButtonContainer>
 				</MainWrapper>
 			</PaperLayout>
 		);
