02cec547c66001fe2247e66789cbaca0b25f216f
Johan Mulyono <johan@paramation.com>
2018-03-01 08:53:18 +0700

    Removed border and outline glow for burger icon menu

--- src/layouts/TopNavBar/styledComponents.tsx
+++ src/layouts/TopNavBar/styledComponents.tsx
@@ -35,7 +35,7 @@ export const BrandLogo = styled.img`
 `;
 
 export const StyledNavbarToggler = styled(NavbarToggler)`
-	border: 0;
+	border: 0 !important;
 
 	& > .navbar-toggler-icon {
 		background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 255, 255, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E") !important;
