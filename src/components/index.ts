947ed19b97a78327a365d944952dd8e91a4df0bc
Johan Mulyono <johan@paramation.com>
2018-03-09 06:51:32 +0700

    Removed unused component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export * from './Form';
 export * from './Header';
-export * from './SmallDescription';
 export * from './ConfirmationModal';
 export * from './StyledComponents';
 export * from './ScrollToTopOnMount';
