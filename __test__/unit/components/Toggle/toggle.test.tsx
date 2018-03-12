9f7cdaf649ae144929f7d920868927d9a44fcc5f
Johan Mulyono <johan@paramation.com>
2018-03-12 11:52:46 +0700

    Fixed Toggle testing

--- __test__/unit/components/Toggle/toggle.test.tsx
+++ __test__/unit/components/Toggle/toggle.test.tsx
@@ -1,27 +1,50 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { Toggle } from '../../../../src/components/Toggle';
+import { ToggleLabel, ToggleName } from '../../../../src/components/Toggle/styledComponents';
 import ReactToggle from 'react-toggle';
 
 describe('Toggle Component (Unit)', () => {
-  test('calls onChange prop on click', () => {
-    const props = {
-      disabled: false,
-      name: 'Some Toggle',
-      label: 'Some Toggle',
-      checked: false,
-      onChange: jest.fn(),
-    }
+	let wrapper;
+	const props = {
+		disabled: false,
+		name: 'Some Toggle',
+		label: 'Some Toggle',
+		checked: false,
+		onChange: jest.fn()
+	};
 
-    const toggle = shallow(<Toggle {... props} />);
+	beforeAll(() => {
+		wrapper = shallow(<Toggle {... props} />);
+	});
+
+	it('should render the component', () => {
+		expect(wrapper.length).toEqual(1);
+	});
+
+	it('should render a <ReactToggle /> component', () => {
+		expect(wrapper.find(ReactToggle).length).toEqual(1);
+	});
+
+	it('should render a <ToggleLabel /> component', () => {
+		expect(wrapper.find(ToggleLabel).length).toEqual(1);
+	});
+
+	it('should render a <ToggleName /> component inside <ToggleLabel />', () => {
+		expect(wrapper.find(ToggleLabel).find(ToggleName).length).toEqual(1);
+	});
+
+	it('<ToggleName /> should have the correct content', () => {
+		expect(wrapper.find(ToggleLabel).find(ToggleName).get(0).props.children).toBe(props.label);
+	});
+
+  test('calls onChange prop on click', () => {
     const event = {
       currentTarget: {
         checked: true
       }
-    }
-
-    toggle.instance().handleChange(event);
-
+    };
+    wrapper.instance().handleChange(event);
     expect(props.onChange.mock.calls.length).toBe(1);
   });
 });
