92502173690ae955d660748407237b50350f61d5
Johan Mulyono <johan@paramation.com>
2018-03-28 14:28:19 +0700

    Fix DebtOrderRow testing

--- __test__/unit/modules/Dashboard/Debts/DebtOrderHistory/DebtOrderRow.test.tsx
+++ __test__/unit/modules/Dashboard/Debts/DebtOrderHistory/DebtOrderRow.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { DebtOrderRow } from '../../../../../../src/modules/Dashboard/Debts/DebtOrderHistory/DebtOrderRow';
-import { shortenString, amortizationUnitToFrequency, debtOrderFromJSON } from '../../../../../../src/utils';
+import { shortenString, amortizationUnitToFrequency } from '../../../../../../src/utils';
 import {
 	StyledRow,
 	Drawer,
@@ -14,25 +14,31 @@ import { Col, Collapse } from 'reactstrap';
 
 describe('<DebtOrderRow />', () => {
 	const debtOrder = {
-		json: "{\"principalToken\":\"0x9b62bd396837417ce319e2e5c8845a5a960010ea\",\"principalAmount\":\"10\",\"termsContract\":\"0x1c907384489d939400fa5c6571d8aad778213d74\",\"termsContractParameters\":\"0x0000000000000000000000000000008500000000000000000000000000000064\",\"kernelVersion\":\"0x89c5b853e9e32bf47c7da1ccb02e981b74c47f2f\",\"issuanceVersion\":\"0x1d8e76d2022e017c6c276b44cb2e4c71bd3cc3de\",\"debtor\":\"0x431194c3e0f35bc7f1266ec6bb85e0c5ec554935\",\"debtorFee\":\"0\",\"creditor\":\"0x431194c3e0f35bc7f1266ec6bb85e0c5ec554935\",\"creditorFee\":\"0\",\"relayer\":\"0x0000000000000000000000000000000000000000\",\"relayerFee\":\"0\",\"underwriter\":\"0x0000000000000000000000000000000000000000\",\"underwriterFee\":\"0\",\"underwriterRiskRating\":\"0\",\"expirationTimestampInSec\":\"1524613355\",\"salt\":\"0\",\"debtorSignature\":{\"v\":27,\"r\":\"0xc5c0aaf7b812cb865aef48958e2d39686a13c292f8bd4a82d7b43d833fb5047d\",\"s\":\"0x2fbbe9f0b8e12ed2875905740fa010bbe710c3e0c131f1efe14fb41bb7921788\"},\"creditorSignature\":{\"v\":27,\"r\":\"0xc5c0aaf7b812cb865aef48958e2d39686a13c292f8bd4a82d7b43d833fb5047d\",\"s\":\"0x2fbbe9f0b8e12ed2875905740fa010bbe710c3e0c131f1efe14fb41bb7921788\"},\"underwriterSignature\":{\"r\":\"\",\"s\":\"\",\"v\":0}}",
-		principalTokenSymbol: 'REP',
-		description: 'Hello, Can I borrow some REP please?',
-		issuanceHash: '0x89e9eac37c5f14b657c69ccd891704b3236b84b9ca1d449bd09c5fbaa24afebf',
-		fillLoanShortUrl: 'http://bit.ly/2I4bahM',
-		repaidAmount: new BigNumber(4),
-		termLength: new BigNumber(100),
-		interestRate: new BigNumber(12.3),
+		debtor: '0x431194c3e0f35bc7f1266ec6bb85e0c5ec554935',
+		termsContract: '0x1c907384489d939400fa5c6571d8aad778213d74',
+		termsContractParameters: '0x0000000000000000000000000000008500000000000000000000000000000064',
+		underwriter: '0x0000000000000000000000000000000000000000',
+		underwriterRiskRating: new BigNumber(0),
 		amortizationUnit: 'hours',
-		status: 'active'
+		interestRate: new BigNumber(3.12),
+		principalAmount: new BigNumber(100),
+		principalTokenSymbol: 'REP',
+		termLength: new BigNumber(6),
+		issuanceHash: '0x89e9eac37c5f14b657c69ccd891704b3236b84b9ca1d449bd09c5fbaa24afebf',
+		repaidAmount: new BigNumber(4),
+		repaymentSchedule: [1553557371],
+		status: 'active',
+		json: '{\"principalToken\":\"0x9b62bd396837417ce319e2e5c8845a5a960010ea\",\"principalAmount\":\"10\",\"termsContract\":\"0x1c907384489d939400fa5c6571d8aad778213d74\",\"termsContractParameters\":\"0x0000000000000000000000000000008500000000000000000000000000000064\",\"kernelVersion\":\"0x89c5b853e9e32bf47c7da1ccb02e981b74c47f2f\",\"issuanceVersion\":\"0x1d8e76d2022e017c6c276b44cb2e4c71bd3cc3de\",\"debtor\":\"0x431194c3e0f35bc7f1266ec6bb85e0c5ec554935\",\"debtorFee\":\"0\",\"creditor\":\"0x431194c3e0f35bc7f1266ec6bb85e0c5ec554935\",\"creditorFee\":\"0\",\"relayer\":\"0x0000000000000000000000000000000000000000\",\"relayerFee\":\"0\",\"underwriter\":\"0x0000000000000000000000000000000000000000\",\"underwriterFee\":\"0\",\"underwriterRiskRating\":\"0\",\"expirationTimestampInSec\":\"1524613355\",\"salt\":\"0\",\"debtorSignature\":{\"v\":27,\"r\":\"0xc5c0aaf7b812cb865aef48958e2d39686a13c292f8bd4a82d7b43d833fb5047d\",\"s\":\"0x2fbbe9f0b8e12ed2875905740fa010bbe710c3e0c131f1efe14fb41bb7921788\"},\"creditorSignature\":{\"v\":27,\"r\":\"0xc5c0aaf7b812cb865aef48958e2d39686a13c292f8bd4a82d7b43d833fb5047d\",\"s\":\"0x2fbbe9f0b8e12ed2875905740fa010bbe710c3e0c131f1efe14fb41bb7921788\"},\"underwriterSignature\":{\"r\":\"\",\"s\":\"\",\"v\":0}}',
+		creditor: '0x431194c3e0f35bc7f1266ec6bb85e0c5ec554935',
+		description: 'Hello, Can I borrow some REP please?',
+		fillLoanShortUrl: 'http://bit.ly/2I4bahM'
 	};
 
 	describe('#render', () => {
 		let wrapper;
 		let props;
-		let debtOrderInfo;
 		beforeEach(() => {
 			props = { debtOrder };
-			debtOrderInfo = debtOrderFromJSON(props.debtOrder.json);
 			wrapper = shallow(<DebtOrderRow {... props} />);
 		});
 
@@ -59,7 +65,7 @@ describe('<DebtOrderRow />', () => {
 			});
 
 			it('1st <Col /> should render principal info', () => {
-				const content = debtOrderInfo.principalAmount.toNumber() + ' ' + props.debtOrder.principalTokenSymbol;
+				const content = debtOrder.principalAmount.toNumber() + ' ' + props.debtOrder.principalTokenSymbol;
 				expect(styledRow.find(Col).at(0).get(0).props.children).toEqual(content);
 			});
 
@@ -70,7 +76,7 @@ describe('<DebtOrderRow />', () => {
 
 			it('3rd <Col /> should render status info', () => {
 				expect(styledRow.find(Col).at(2).get(0).props.children).toEqual('Delinquent');
-				props.debtOrder.repaidAmount = debtOrderInfo.principalAmount;
+				props.debtOrder.repaidAmount = debtOrder.principalAmount;
 				wrapper.setProps(props);
 				styledRow = wrapper.find(StyledRow);
 				expect(styledRow.find(Col).at(2).get(0).props.children).toEqual('Paid');
