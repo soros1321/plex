55b8a388ca65542d48a8f5398ed7995a3c0b95b4
Nadav Hollander <nadav@dharma.io>
2018-03-28 18:19:11 -0700

    add mangle exception for BigNumber

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -102,7 +102,7 @@ module.exports = {
       '.jsx',
     ],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -279,6 +279,7 @@ module.exports = {
       },
       mangle: {
         safari10: true,
+        except: ["BigNumber"]
       },
       output: {
         comments: false,
