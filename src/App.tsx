1dafed2a327a87a29bd57c3dcdb9d8304185ea09
Nadav Hollander <nadav@dharma.io>
2018-03-30 11:58:20 -0700

    Add GA tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,68 @@
-import * as React from 'react';
-import { PageLayout } from './layouts';
-import { ToastContainer } from './components';
-import * as Web3 from 'web3';
-const promisify = require('tiny-promisify');
+import * as React from "react";
+import { PageLayout } from "./layouts";
+import { ToastContainer } from "./components";
+import * as Web3 from "web3";
+import * as ReactGA from "react-ga";
+
+const promisify = require("tiny-promisify");
 
 interface Props {
-	web3: Web3;
-	accounts: string[];
+    web3: Web3;
+    accounts: string[];
 }
 
 interface State {
-	intervalId: any;
+    intervalId: any;
 }
 
+// Initialize Google Analytics
+ReactGA.initialize("UA-98616406-4");
+
 class App extends React.Component<Props, State> {
-	constructor(props: Props) {
-		super(props);
+    constructor(props: Props) {
+        super(props);
 
-		this.state = {
-			intervalId: undefined
-		};
-	}
+        this.state = {
+            intervalId: undefined,
+        };
+    }
 
-	componentDidMount() {
-		const intervalId = setInterval(
-			() => this.checkAccount(this.props.web3, this.props.accounts),
-			1000
-		);
-		this.setState({ intervalId });
-	}
+    componentDidMount() {
+        const intervalId = setInterval(
+            () => this.checkAccount(this.props.web3, this.props.accounts),
+            1000,
+        );
+        this.setState({ intervalId });
+    }
 
-	componentWillUnmount() {
-		clearInterval(this.state.intervalId);
-	}
+    componentWillUnmount() {
+        clearInterval(this.state.intervalId);
+    }
 
-	componentWillReceiveProps(nextProps: Props) {
-		if (nextProps.web3 && nextProps.accounts) {
-			this.checkAccount(nextProps.web3, nextProps.accounts);
-		}
-	}
+    componentWillReceiveProps(nextProps: Props) {
+        if (nextProps.web3 && nextProps.accounts) {
+            this.checkAccount(nextProps.web3, nextProps.accounts);
+        }
+    }
 
-	async checkAccount(web3: Web3, accounts: string[]) {
-		if (!web3 || !accounts) {
-			return;
-		}
-		const latestAccounts = await promisify(web3.eth.getAccounts)();
-		if (latestAccounts.length && accounts.length && latestAccounts[0] !== accounts[0]) {
-			window.location.reload();
-		}
-	}
+    async checkAccount(web3: Web3, accounts: string[]) {
+        if (!web3 || !accounts) {
+            return;
+        }
+        const latestAccounts = await promisify(web3.eth.getAccounts)();
+        if (latestAccounts.length && accounts.length && latestAccounts[0] !== accounts[0]) {
+            window.location.reload();
+        }
+    }
 
-	render() {
-		return (
-			<PageLayout>
-				<ToastContainer />
-				{this.props.children}
-			</PageLayout>
-		);
-	}
+    render() {
+        return (
+            <PageLayout>
+                <ToastContainer />
+                {this.props.children}
+            </PageLayout>
+        );
+    }
 }
 
 export { App };
