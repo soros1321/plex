39639623e7dc221a392740f6201eab801900c3a3
Johan Mulyono <johan@paramation.com>
2018-04-10 16:13:30 +0700

    Added validation for termLength and interestRate on Request Order form

---
+++ src/modules/RequestLoan/RequestLoanForm/validator.ts
@@ -0,0 +1,26 @@
+export const validateTermLength = (termLength: number) => {
+	const maxAmount = 65535;
+	let error: string = "";
+	if (termLength % 1 !== 0) {
+		error = "Term length value cannot have decimals";
+	} else if (termLength < 0) {
+		error = "Term length value cannot be negative";
+	} else if (termLength > maxAmount) {
+		error = `Term length value cannot be greater than ${maxAmount}`;
+	}
+	return error;
+};
+
+export const validateInterestRate = (interestRate: number) => {
+	const maxAmount = 1677.7216;
+	const maxDecimalPlaces = 4;
+	let error: string = "";
+	if (interestRate < 0) {
+		error = "Interest amount cannot be negative";
+	} else if (interestRate > maxAmount) {
+		error = `Interest amount cannot be greater than ${maxAmount}`;
+	} else if (interestRate % 1 !== 0 && interestRate.toString().split(".")[1].length > maxDecimalPlaces) {
+		error = `Interest amount cannot have more than ${maxDecimalPlaces} decimal places`;
+	}
+	return error;
+};
