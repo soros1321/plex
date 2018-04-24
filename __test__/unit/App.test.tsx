c6b1eeef409f740490589e4cd82224d332b4d47f
Kayvon Tehranian <ktehranian@gmail.com>
2018-04-24 16:55:50 -0700

    refactor: clean up testing of our app component

--- __test__/unit/App.test.tsx
+++ __test__/unit/App.test.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
+import { App } from "../../src/App";
+import MockWeb3 from "../../__mocks__/web3";
 import { shallow } from "enzyme";
-import { App } from "src/App";
-import MockWeb3 from "__mocks__/web3";
 jest.mock("react-ga");
 
-it("renders without crashing", () => {
-    const props = {
-        web3: new MockWeb3(),
-        accounts: ["account1"],
-    };
-    shallow(<App {...props} />);
+describe("<App />", () => {
+    it("renders without crashing", () => {
+        const props = {
+            web3: new MockWeb3(),
+            accounts: ["account1"],
+        };
+        shallow(<App {...props} />);
+    });
 });
